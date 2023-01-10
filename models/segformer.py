462a66d1c0b5165dbe435d935a946e51cea90ca7
Johannes Jestram <17967115+jo-jstrm@users.noreply.github.com>
2023-01-10 18:55:41 +0100

    Remove unused import

--- models/segformer.py
+++ models/segformer.py
@@ -2,7 +2,6 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from functools import partial
 
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 from timm.models.registry import register_model
